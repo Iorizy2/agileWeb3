c38b49c395741465fbbaab36627e8c55d215fe8d
Iorizy2 <374545365@qq.com>
2018-05-23 18:08:52 +0800

    first commit

---
+++ src/component/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import Header from './Header';
+//import MainSection from './MainSection'
+
+const initialState = [
+  {
+    text: 'React ES6 TodoMVC',
+    completed: false,
+    id:0
+  }
+]
+/*
+const todos = [
+  {
+    id:this.state.todos.reduce((maxId,todo) =>Math.max(todo.id,maxId), -1)+1,
+    completed: false,
+    text: initialState.text
+  },
+  ...this.state.todos
+]
+this.setState({todos})
+*/
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      todos: initialState,
+      gameID:null,
+      player:0
+    }
+  }
+  render(){
+    return(
+      <div>
+        <Header
+         onSave={this.onSave.bind(this)}
+         />
+        
+       
+      </div>
+    );
+  }
+}
+
+export default App;
+// <MainSection todos={this.state.todos}/>
